2b484a548aeea9976a40846746caf38a268c14b2
Gregory <grego525@hotmail.com>
2021-12-16 18:29:13 +0100

    first commit

---
+++ ExceptionsPerformances/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ExceptionsPerformances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<HandleExceptionsBenchmark>();
+        }
+
+    }
+
+    public class HandleExceptions
+    {
+        public void Test()
+        {
+            try
+            {
+                ThrowException();
+            }
+            catch (Exception ex)
+            {
+
+                //Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void ThrowException()
+        {
+            throw new Exception("Simulate exception");
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class HandleExceptionsBenchmark
+    {
+        private static readonly HandleExceptions handleExceptions = new HandleExceptions();
+
+        [Benchmark]
+        public void Test()
+        {
+            handleExceptions.Test();
+        }
+    }
+}
